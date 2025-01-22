57c4df57c56539943f93103c5500d058ab79d912
pranav <pranav@techbla.in>
2025-01-22 23:34:02 +0530

    header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+import Script from 'next/script'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
-
 import { cn } from '@/lib/utils';
 import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@/components/ui/toaster';
@@ -12,26 +12,32 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Companion.AI',
-  description: 'Your customized companion.',
+ title: 'Companion.AI',
+ description: 'Your customized companion.',
 }
 
 export default function RootLayout({
-  children,
+ children,
 }: {
-  children: React.ReactNode
+ children: React.ReactNode
 }) {
-  return (
-    <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
-        <body className={cn("bg-secondary", inter.className)}>
-          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-            <ProModal />
-            {children}
-            <Toaster />
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
-  )
-}
+ return (
+   <ClerkProvider>
+     <html lang="en" suppressHydrationWarning>
+       <head>
+         <Script 
+           src="https://checkout.razorpay.com/v1/checkout.js"
+           strategy="beforeInteractive"
+         />
+       </head>
+       <body className={cn("bg-secondary", inter.className)}>
+         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+           <ProModal />
+           {children}
+           <Toaster />
+         </ThemeProvider>
+       </body>
+     </html>
+   </ClerkProvider>
+ )
+} 
