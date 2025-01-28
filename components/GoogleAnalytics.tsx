6f088c72a193c6f606822e26aac1be7f00d0c973
pranav <pranav@techbla.in>
2025-01-28 22:05:24 +0530

    header

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -7,17 +7,26 @@ export default function GoogleAnalytics() {
     <>
       {/* Google tag (gtag.js) */}
       <Script 
-        async 
         src="https://www.googletagmanager.com/gtag/js?id=G-29827YJ860"
+        strategy="afterInteractive"
+        onLoad={() => {
+          console.log('GA script loaded')
+        }}
       />
       <Script
         id="google-analytics"
+        strategy="afterInteractive"
+        onLoad={() => {
+          console.log('GA config loaded')
+        }}
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-29827YJ860');
+            gtag('config', 'G-29827YJ860', {
+              page_path: window.location.pathname,
+            });
           `,
         }}
       />
