7390ce4cd34b1777107742a1b3dc1af1b3b7d95d
pranav <pranav@techbla.in>
2025-02-12 22:35:33 +0530

    header

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,4 +1,5 @@
 import { Menu } from "lucide-react";
+
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Sidebar } from "@/components/sidebar";
 
@@ -12,13 +13,8 @@ export const MobileSidebar = ({
       <SheetTrigger className="md:hidden pr-4">
         <Menu />
       </SheetTrigger>
-      <SheetContent 
-        side="left" 
-        className="p-0 w-[280px] overflow-y-auto overflow-x-hidden" // Fixed width and hide horizontal scroll
-      >
-        <div className="flex flex-col h-full w-full">
-          <Sidebar isPro={isPro} />
-        </div>
+      <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
+        <Sidebar isPro={isPro} />
       </SheetContent>
     </Sheet>
   );
